5c26b70d2edf1ef445de5752a955537fbd0c7dc2
nullptr <iateurpanda@gmail.com>
2019-07-02 08:26:27 -0400

    added pywal config

--- comfyi3Arch.sh
+++ comfyi3Arch.sh
@@ -4,5 +4,8 @@ URL="https://raw.githubusercontent.com/DWShuo/.dotfiles/master/.config/i3/config
 mv ~/.config/i3/config ~/.config/i3/config.bak
 wget $URL -P ~/.config/i3/
 yay -Syu steam spotify python-pywal
+echo 'Setting up pywal'
+echo $'(cat ~/.cache/wal/sequences &)' >> '.zshrc'
+echo $'wal -R' > '.xinitrc'
 rm .config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
 pkill -KILL -u $USER
