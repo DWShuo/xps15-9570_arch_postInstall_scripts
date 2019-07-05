c2d0bdc910492e3b753a2e59f75e8cb830557af1
nullptr <iateurpanda@gmail.com>
2019-07-04 22:11:45 -0400

    + dotfile install

--- comfyi3Arch.sh
+++ comfyi3Arch.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 currentUser=$USER
-echo 'Configuring i3-gaps'
-URL="https://raw.githubusercontent.com/DWShuo/.dotfiles/master/.config/i3/config"
-mv /home/$currentUser/.config/i3/config /home/$currentUser/.config/i3/config.bak
-wget $URL -P /home/$currentUser/.config/i3/
 echo 'Installling steam spotify pywal bluetooth tlp ncdu...'
-yay -Syu steam python-pywal blueman bluez tlp ncdu
+yay -Syu steam python-pywal blueberry tlp ncdu 
 echo 'Setting up pywal'
 echo $'(cat ~/.cache/wal/sequences &)' >> '.zshrc'
 echo $'wal -R' > '.xinitrc'
-sudo -u root systemctl enable bluetooth.service
+#sudo -u root systemctl enable bluetooth.service
 echo 'Setting up tlp'
 sudo -u root tlp start
 sudo -u root systemctl enable tlp.service
 sudo -u root systemctl enable tlp-sleep.service
 sudo -u root systemctl mask systemd-rfkill.service
 sudo -u root systemctl mask systemd-rfkill.socket
+echo 'Setting up mpd group'
+sudo -u root gpasswd -a mpd users
+sudo -u root chmod 710 ~/
+sudo -u root gpasswd -a mpd audio
+echo 'Restoring dotfiles'
+git clone https://github.com/DWShuo/dotfiles.git
+chmod u+x restore.sh
+/bin/bash /home/$currentUser/dotfiles/restore.sh
 
-echo 'Logging out to apply settings'
-sleep 3s
-pkill -KILL -u $USER
+#echo 'Logging out to apply settings'
+#sleep 3s
+#pkill -KILL -u $USER
